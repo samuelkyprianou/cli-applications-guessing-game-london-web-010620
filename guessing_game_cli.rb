b9209b785a99bee9379c24254b9ec57a517fe985
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-21 18:02:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
     puts "Goodbye!"
   elsif num != input
   puts "Sorry! The computer guessed #{num}."
-else
+elsif num == input
   puts "You guessed the correct number!"
 end
 end
