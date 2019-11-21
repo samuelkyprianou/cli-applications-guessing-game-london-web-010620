0f2f5c417d3b63d3dafa0d841bbcfed433170977
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-21 18:05:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,11 +1,11 @@
 def run_guessing_game
   num = 1+rand(7)
   input = gets.chomp.to_s
-elsif num == input
+if num == input
   return "You guessed the correct number!"
-  if input == 'exit'
-    puts "Goodbye!"
-  elsif num != input
+elsif num != input
   return "Sorry! The computer guessed #{num}."
+elsif input == 'exit'
+    puts "Goodbye!"
 end
 end
